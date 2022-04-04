b1f48dccba62be3612963692b37410ab559c8942
nicksorenson <nicksorenson99@gmail.com>
2022-04-04 17:44:39 -0600

    Working on menuing, changed some style stuff

---
+++ scripts/screens/gamePlay.js
@@ -0,0 +1,12 @@
+MyGame.screens['gamePlayScreen'] = (function(){
+    function run(){
+        // do nothing for now
+    }
+    function initialize(){
+        // do nothing for now
+    }
+    return {
+        run: run,
+        initialize: initialize
+    }
+}())
