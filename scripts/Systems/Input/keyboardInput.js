236d298490dec6f67d9216278c1ea370593022f8
Nick Sorenson <nicksorenson99@gmail.com>
2022-04-13 18:50:44 -0600

    is you logic is a go

--- scripts/Systems/Input/keyboardInput.js
+++ scripts/Systems/Input/keyboardInput.js
@@ -6,6 +6,22 @@
 //
 // --------------------------------------------------------------
 MyGame.systems.keyboardInput = (function () {
+
+    'use strict';
+    let keysDown = {};
+
+    function keyPress(e) {
+        e.preventDefault();
+        keysDown[e.key] = e.timeStamp;
+    }
+
+    function keyRelease(e) {
+        delete keysDown[e.key];
+    }
+    function keyRelease2(key) {
+        delete keysDown[key];
+    }
+
     function addInputComponent(entity) {
         entity.addComponent(MyGame.components.KeyboardControlled({
             keys: {
@@ -26,54 +42,75 @@ MyGame.systems.keyboardInput = (function () {
             }
         ))
     }
-    'use strict';
-    let keysDown = {};
-
-    function keyPress(e) {
-        e.preventDefault();
-        keysDown[e.key] = e.timeStamp;
+    // function
+    function moveAll(entities, key, input){
+        for(let id in entities){
+            let entity = entities[id];
+            if(entity.components.movable){
+                entity.components.movable.facing = input.keys[key];
+                entity.components.movable.moveDirection = input.keys[key];
+            }
+        }
     }
-
-    function keyRelease(e) {
-        delete keysDown[e.key];
+    function doMove(entities) {
+        for (let id in entities) {
+            let entity = entities[id];
+            if (entity.components['keyboard-controlled']) {
+                let input = entity.components['keyboard-controlled'];
+                for (let key in input.keys) {
+                    if (keysDown[key]) {
+                        moveAll(entities, key, input);
+                        keyRelease2(key);
+                    }
+                }
+            }
+        }
     }
-    function keyRelease2(key) {
-        delete keysDown[key];
+    function checkIsYou(entities){
+        for (let id in entities) {
+            let entity = entities[id];
+            if (entity.components.properties) {
+                if (entity.components.properties.is('YOU')) {
+                    // console.log(`Entity is you: ${entity.components.noun.valueType}`)
+                    addInputComponent(entity);
+                }
+                else {
+                    // console.log("hello");
+                    // if (entity.components['keyboard-controlled']) {
+                    //     console.log(`Removing keyboard input from entity:`)
+                    //     entity.removeComponent(entity.components['keyboard-controlled']);
+                    //     console.log(entity);
+                    // }
+                    // console.log('hello')
+                    // console.log(entity.components.movable);
+                }
+            }
+            else{
+                if(entity.components.movable) {
+                    console.log("removing moving component")
+                    entity.removeComponent(entity.components.movable);
+                }
+            }
+        }   
     }
-
     // --------------------------------------------------------------
     //
     // Public interface used to update entities based on keyboard input.
     //
     // --------------------------------------------------------------
     function update(elapsedTime, entities) {
-        for (let id in entities) {
-            let entity = entities[id];
-            if (entity.components.properties) {
-                if (entity.components.properties.is('YOU')) {
-                    // console.log(entitycomponents.properties)
-                    addInputComponent(entity);
-                    if (entity.components['keyboard-controlled']) {
-                        let input = entity.components['keyboard-controlled'];
-                        for (let key in input.keys) {
-                            if (keysDown[key]) {
-                                entity.components.movable.facing = input.keys[key];
-                                entity.components.movable.moveDirection = input.keys[key];
-                                keyRelease2(key);
-                            }
-                        }
-                    }
-                }
-                else {
-                    if (entity.components['keyboard-controlled']) {
-                        entity.removeComponent(entity.components['keyboard-controlled'])
-                        // entity.removeComponent(entity.components.movable)
-                    }
-                }
-            }
+        // for (let id in entities) {
+        //     let entity = entities[id];
+        //     checkIsYou(entity);
+        // }
+        checkIsYou(entities);
+        doMove(entities);
+        // for(let id in entities) {
+        //     let entity = entities[id];
+        //     doMove(entities, entity);
+        // }
 
-
-        }
+        
     }
 
     window.addEventListener('keydown', keyPress);
